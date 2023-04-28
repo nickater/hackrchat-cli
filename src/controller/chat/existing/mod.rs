3a5e859463b15ebd0005cec5cae038c7226c10b3
Nick Ater <30247698+nickater@users.noreply.github.com>
2023-04-27 23:26:51 -0400

    clean up

--- src/controller/chat/existing/mod.rs
+++ src/controller/chat/existing/mod.rs
@@ -1,10 +1,9 @@
 use std::{env, fs};
 
-use super::{existing, new};
-use crate::{ui, utils::clear};
+use crate::ui;
 
 pub fn existing_chat_controller() {
-    let mut is_running = true;
+    let is_running = true;
     // fetch exiting chats from database and put them in a vector
     while is_running {
         let options: Vec<&str> = vec!["nater", "gus22", "staceyk"];
